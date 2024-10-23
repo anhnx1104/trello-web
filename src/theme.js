b92a12ce74489d3bcb6e9bb75c83063d506e772b
anhnx4 <anh833905@gmail.com>
2024-10-23 15:07:54 +0700

    add mode

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import { deepOrange, teal, cyan, orange } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
+      //   spacing: (factor) => `${0.25 * factor}rem`,
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+      //   spacing: (factor) => `${0.25 * factor}rem`,
+    },
+  },
+  // ...other properties
+});
+
+export default theme;
