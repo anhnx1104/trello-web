b234b037a433f5987e0434bdd3adf87408c68afa
anhnx4 <anh833905@gmail.com>
2024-10-25 14:18:24 +0700

    directory structure

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,9 +1,7 @@
-import * as React from "react";
-
 import Container from "@mui/material/Container";
-import AppBar from "~/components/AppBar";
-import BoardBar from "./BoardBar";
-import BoardContent from "./BoardContent";
+import AppBar from "~/components/AppBar/AppBar";
+import BoardBar from "./BoardBar/BoardBar";
+import BoardContent from "./BoardContent/BoardContent";
 
 const BoardDetails = () => {
   return (
