d18ba8297f13e71df5592267aad59875a6679405
anhnx4 <anh833905@gmail.com>
2024-10-28 17:41:12 +0700

    handle drag card to columns

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -28,6 +28,7 @@ const Cards = (props) => {
     transform: CSS.Translate.toString(transform),
     transition,
     opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? "1px solid #2ecc71" : undefined,
   };
 
   const shouldShowCardActions = () => {
